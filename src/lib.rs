7283691ed9accbae647637422e761e6b36f42fc8
Benjamin Levy <blevy@protonmail.com>
2019-11-24 03:16:39 -0500

    Add stub

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,5 @@
-
+/// Solve for an assertion failure
+/// - **Return value**: stdin bytes that cause an assertion failure, if found
+pub fn solve_assert_fail(prog: llvm_ir::Module) -> Option<Vec<u8>> {
+    unimplemented!()
+}
