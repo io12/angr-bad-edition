10bb7a7d44122c7c0ee62764c682cb513f3ca8ab
Benjamin Levy <blevy@protonmail.com>
2019-11-24 03:20:24 -0500

    This is harder than I expected

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
 /// Solve for an assertion failure
+/// - **Parameters**
+///   - `prog`: program to solve
+///   - `entry_name`: name of entry point function
 /// - **Return value**: stdin bytes that cause an assertion failure, if found
-pub fn solve_assert_fail(prog: llvm_ir::Module) -> Option<Vec<u8>> {
+pub fn solve_assert_fail(prog: llvm_ir::Module, entry_name: &str) -> Option<Vec<u8>> {
+    let entry_func = prog.functions.iter().find(|func| func.name == entry_name)?;
     unimplemented!()
 }
