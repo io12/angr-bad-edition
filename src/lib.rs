2f0155117578864d10e5a7f889f1004fe4c9cb97
Benjamin Levy <blevy@protonmail.com>
2019-11-24 16:05:25 -0500

    Disassembles shellcode

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,7 @@
+pub mod lift;
+
 /// Solve for an assertion failure
-/// - **Parameters**
-///   - `prog`: program to solve
-///   - `entry_name`: name of entry point function
 /// - **Return value**: stdin bytes that cause an assertion failure, if found
-pub fn solve_assert_fail(prog: llvm_ir::Module, entry_name: &str) -> Option<Vec<u8>> {
-    let entry_func = prog.functions.iter().find(|func| func.name == entry_name)?;
+pub fn solve_assert_fail() -> Option<Vec<u8>> {
     unimplemented!()
 }
